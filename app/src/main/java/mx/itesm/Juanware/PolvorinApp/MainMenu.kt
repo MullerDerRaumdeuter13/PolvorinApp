c00f8cca7af23e911d7b5fa4e2745b7e8fbe6e3e
Eduardo Müller <a01745219@itesm.mx>
2021-04-13 19:59:51 -0500

    MainMenu creation y login refactor

--- app/src/main/java/mx/itesm/Juanware/PolvorinApp/MainMenu.kt
+++ app/src/main/java/mx/itesm/Juanware/PolvorinApp/MainMenu.kt
@@ -3,9 +3,9 @@ package mx.itesm.Juanware.PolvorinApp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class MainMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main_menu)
     }
 }
