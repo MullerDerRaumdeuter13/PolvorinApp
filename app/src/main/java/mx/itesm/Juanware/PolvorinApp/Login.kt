70d8ad5ecbbde9db6627cfc8957571016332ac91
FernandoSSilvaM <a01746925@itesm.mx>
2021-04-14 15:08:01 -0500

    Se agrego Authentification la proyecto

--- app/src/main/java/mx/itesm/Juanware/PolvorinApp/Login.kt
+++ app/src/main/java/mx/itesm/Juanware/PolvorinApp/Login.kt
@@ -1,9 +1,16 @@
 package mx.itesm.Juanware.PolvorinApp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.database.FirebaseDatabase
 
+
+/*
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,6 +20,7 @@ class Login : AppCompatActivity() {
 
 
     }
+*/
 /*    private fun probarEscritura() {
         // Write a message to the database
         // Write a message to the database
@@ -24,6 +32,78 @@ class Login : AppCompatActivity() {
 
 
         println("TERMINANDO ESCRITURAasdfasdfasdf")
-    }*/
+    }*//*
 
+
+}*/
+class Login : AppCompatActivity() {
+    private lateinit var mAuth: FirebaseAuth
+    private val RC_SIGN_IN = 200
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        mAuth = FirebaseAuth.getInstance()
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        val usuario = mAuth.currentUser
+        if (usuario != null){
+            //ya esta firmado
+            println("BIENVENIDO DE VUELTA: ${usuario.displayName}")
+            println("CORREO: ${usuario.email}")
+            println("ID: ${usuario.uid}")
+            //Lanzar a la segunda pantalla
+        }else {
+            println("hacer login...")
+        }
+    }
+
+    fun autenticar(v: View){
+        autenticar()
+    }
+
+    fun hacerLogOut(v: View){
+        AuthUI.getInstance().signOut(this)
+    }
+
+    fun autenticar(){
+        val proveedores = arrayListOf(
+                AuthUI.IdpConfig.GoogleBuilder().build()
+        )
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(proveedores)
+                        .build(),
+                RC_SIGN_IN)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == RC_SIGN_IN){
+            val response = FirebaseAuth.getInstance().currentUser
+            when(resultCode){
+                RESULT_OK -> {
+                    val usuario = FirebaseAuth.getInstance().currentUser
+                    println("Bienvenido: ${usuario.displayName}")
+                    println("Correo: ${usuario.email}")
+                    println("ID: ${usuario.uid}")
+                    println("Imagen: ${usuario.photoUrl}")
+                    //Lanzar la siguiente actividad.
+                }
+                RESULT_CANCELED -> {
+                    println("Cancelado (back)")
+                }
+                else -> {
+                    val response = IdpResponse.fromResultIntent(data)
+                    println("Error: ${response?.error?.errorCode}")
+                }
+            }
+        }
+    }
 }
