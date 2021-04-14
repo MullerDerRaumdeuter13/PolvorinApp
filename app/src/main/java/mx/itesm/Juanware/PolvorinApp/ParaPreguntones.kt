0ebad9d40e24ccaff97e4acfc001100432fb3cca
Eduardo Müller <a01745219@itesm.mx>
2021-04-13 21:26:48 -0500

    Pa quien pregunte

---
+++ app/src/main/java/mx/itesm/Juanware/PolvorinApp/ParaPreguntones.kt
@@ -0,0 +1,11 @@
+package mx.itesm.Juanware.PolvorinApp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ParaPreguntones : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_para_preguntones)
+    }
+}
