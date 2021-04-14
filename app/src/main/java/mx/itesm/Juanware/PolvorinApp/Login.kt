6cac8d3e4e97ce7c2cb0837502c7b32659616afa
Eduardo Müller <a01745219@itesm.mx>
2021-04-13 20:28:19 -0500

    Iconos y elementos del menu

--- app/src/main/java/mx/itesm/Juanware/PolvorinApp/Login.kt
+++ app/src/main/java/mx/itesm/Juanware/PolvorinApp/Login.kt
@@ -2,11 +2,23 @@ package mx.itesm.Juanware.PolvorinApp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.database.FirebaseDatabase
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        probarEscritura()
+    }
+    private fun probarEscritura() {
+        // Write a message to the database
+        // Write a message to the database
+        println("PROBANDO ESCRITURA")
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("/autor")
+        myRef.setValue("Oscar Zuniga Lara A01654827")
+        println("TERMINANDO ESCRITURA")
     }
 
 }
--- app/src/main/java/mx/itesm/Juanware/PolvorinApp/MainMenu.kt
+++ app/src/main/java/mx/itesm/Juanware/PolvorinApp/MainMenu.kt
@@ -1,11 +1,15 @@
 package mx.itesm.Juanware.PolvorinApp
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.database.FirebaseDatabase
+
 
 class MainMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+
+
     }
 }
