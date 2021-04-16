4a64abd13d470c21b4fd06ff921c24a997b0316b
Eduardo Müller <a01745219@itesm.mx>
2021-04-16 03:15:22 -0500

    alpha 1.0.8 Cambio de Menu Principal

--- app/src/main/java/mx/itesm/Juanware/PolvorinApp/MainMenu.kt
+++ app/src/main/java/mx/itesm/Juanware/PolvorinApp/MainMenu.kt
@@ -14,6 +14,7 @@ class MainMenu : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_menu)
+
         configureInitFrag("Profile")
         configMenu()
     }
